0728bc212ff18218a48d009ced2addcce44534be
Amanuel <weditedi25@gmail.com>
2021-01-18 14:03:09 +0000

    Task-2

--- exercises/exercise.js
+++ exercises/exercise.js
@@ -1,4 +1,4 @@
-let mydocument = document;
+
 
 
 /*
@@ -7,6 +7,8 @@ Task 1
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
+
+    
 
     2. the first div element node
     --> should log the ".site-header" node
@@ -18,6 +20,25 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+// Ex-1
+
+let allPara = document.getElementsByTagName("p");
+console.log(`There are ${allPara.length} Paragraphs.`);
+
+//Exe-2
+let firstDiv = document.getElementsByClassName("site-header container");
+console.log(allPara);
+
+//Exe-3
+let jumboText = document.getElementById("jumbotron-text");
+console.log(jumboText);
+
+
+// let primaryListNodes = document.querySelector(".primary-content") -
+let primaryListNodes = document.querySelectorAll(".primary-content")
+
+let pList  = primaryListNodes[0].getElementsByClassName("p");
+console.log(`Number of P tags with in primary-content class: ${pList.length}.`);
 
 /*
 Task 2
@@ -25,6 +46,10 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+let btnAlert = document.getElementById("alertBtn");
+btnAlert.addEventListener("click", function(){
+    alert("Thanks for visiting Bikes for Refugees!");
+})
 
 
 /*
@@ -33,6 +58,12 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+
+let colorChangerBtn = document.getElementById('bgrChangeBtn');
+colorChangerBtn.addEventListener("click", function(e){
+    colorChangerBtn.style.body.backgroundColor = 'blue'; 
+
+})
 
 
 /*
@@ -50,3 +81,5 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+let largerLinkBtn = document.getElementById('largerLinksBtm');
